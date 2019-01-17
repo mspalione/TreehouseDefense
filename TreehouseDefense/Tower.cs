3f262b07122e690dbda107c60f0737aeb6c85845
Melissa Spalione <melissaspalione86@gmail.com>
2019-01-17 14:54:49 -0500

    Added code to Tower.cs

--- TreehouseDefense/Tower.cs
+++ TreehouseDefense/Tower.cs
@@ -1,7 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace TreehouseDefense
 {
     class Tower
     {
-        
+        private const int _range = 1;
+        private const int _power = 1;
+
+        private readonly MapLocation _location;
+
+        public Tower(MapLocation location)
+        {
+            _location = location;
+        }
+
+        public void FireOnInvaders(Invader[] invaders)
+        {
+
+            foreach(Invader invader in invaders)
+            {
+                if(invader.IsActive && _location.InRangeOf(invader.Location, _range))
+                {
+                    invader.DecreaseHealth(_power);
+                    break;
+                }
+            }
+        }
     }
 }
