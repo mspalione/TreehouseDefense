6565f2fc73a321c2f53ed654be25c13d4a512e26
Melissa Spalione <melissaspalione86@gmail.com>
2019-01-17 10:26:44 -0500

    Add project files.

---
+++ TreehouseDefense/Point.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace TreehouseDefense
+{
+    class Point
+    {
+        public readonly int X;
+        public readonly int Y;
+        
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int DistanceTo(int x, int y)
+        {
+            return (int)Math.Sqrt(Math.Pow(X - x, 2) + Math.Pow(Y - y, 2));
+        }
+    }
+}
